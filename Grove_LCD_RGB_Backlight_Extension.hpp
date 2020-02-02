ed1891b501bae26304010048c0767bdc9d10cca1
piete <piete@LAPTOP-3EHRPF9O.fritz.box>
2020-02-02 22:06:28 +0100

    Add class ColorSlider
    Change-Id: Ifb958a492fcbd3ef373aabc25b9bd25b066ddf66

--- Grove_LCD_RGB_Backlight_Extension.hpp
+++ Grove_LCD_RGB_Backlight_Extension.hpp
@@ -25,7 +25,7 @@
 #ifndef __GROVE_LCD_RGB_BACKLIGHT_EXTENSION_HPP__
 #define __GROVE_LCD_RGB_BACKLIGHT_EXTENSION_HPP__
 
-#include <WString.h>
+#include <Arduino.h>
 
 /*
  * Simple Color class.
@@ -140,6 +140,161 @@ public:
 	}
 
 	virtual void slideColor(const Color &color) = 0;
+};
+
+/*
+ * Class ColorSlider allows you to easily "slide" the color of your LCD RGB Backlight,
+ * from e.g. Blue to Red, or from Green to Blue.
+ */
+class ColorSlider {
+private:
+	// from which color to which color must we slide
+	Color _from;
+	Color _to;
+
+	// keep some state
+	Color _current; // current color
+	int _direction = 1; // current direction: up == 1, down == -1
+
+	bool _debugViaSerial = false;
+
+public:
+	ColorSlider(Color from, Color to) :
+			_from(from), _to(to), _current(from) {
+	}
+
+	/*
+	 * Wait for specified number of milliseconds.
+	 * Do this in a loop. Slide a bit, wait a bit.
+	 * The "wait a bit" is "msStepSize" long.
+	 */
+	void delayAndSlide(ColorSliderClient &client, unsigned long ms,
+			unsigned long msStepSize = 5) {
+
+		for (unsigned long i = 0; i < ms; i += msStepSize) {
+			slide(client);
+			delay(msStepSize);
+		}
+
+	}
+
+	void slide(ColorSliderClient &client) {
+		if (_direction == 1) {
+			slideUp(client);
+			return;
+		}
+		if (_direction == -1) {
+			slideDown(client);
+			return;
+		}
+	}
+
+	inline void setDebugViaSerial(bool debugMe) {
+		_debugViaSerial = debugMe;
+	}
+
+private:
+
+	void debugViaSerial() {
+		if (_debugViaSerial) {
+			Serial.print("from:");
+			Serial.print(_from.toString());
+			Serial.print(" / ");
+			Serial.print("current:");
+			Serial.print(_current.toString());
+			Serial.print(" / ");
+			Serial.print("to:");
+			Serial.print(_to.toString());
+			Serial.println();
+		}
+	}
+
+	inline void debugLineViaSerial(const String &str) {
+		if (_debugViaSerial) {
+			Serial.println(str);
+		}
+	}
+
+	inline void reverseDirection() {
+		_direction *= -1;
+	}
+
+	/*
+	 * Going up, we slide: Red, Green, Blue.
+	 */
+	void slideUp(ColorSliderClient &client) {
+		debugViaSerial();
+		if (slideRed(client, _from, _to)) {
+			return;
+		}
+		if (slideGreen(client, _from, _to)) {
+			return;
+		}
+		if (slideBlue(client, _from, _to)) {
+			return;
+		}
+		reverseDirection();
+	}
+
+	/*
+	 * Going down, we slide: Blue, Green, Red.
+	 */
+	void slideDown(ColorSliderClient &client) {
+		debugViaSerial();
+		if (slideBlue(client, _to, _from)) {
+			return;
+		}
+		if (slideGreen(client, _to, _from)) {
+			return;
+		}
+		if (slideRed(client, _to, _from)) {
+			return;
+		}
+		reverseDirection();
+	}
+
+	int slideRed(ColorSliderClient &client, Color &begin, Color &end) {
+		if (begin.red() < end.red() && _current.red() < end.red()) {
+			debugLineViaSerial("increase red");
+			return client.increaseAndSlideColor(1, 0, 0, _current);
+		}
+		if (begin.red() > end.red() && _current.red() > end.red()) {
+			debugLineViaSerial("decrease red");
+			return client.decreaseAndSlideColor(1, 0, 0, _current);
+		}
+		// if equal => nothing to do
+		debugLineViaSerial("red matches");
+		return 0;
+	}
+
+	int slideGreen(ColorSliderClient &client, Color &begin, Color &end) {
+		if (begin.green() < end.green() && _current.green() < end.green()) {
+			debugLineViaSerial("increase green");
+			return client.increaseAndSlideColor(0, 1, 0, _current);
+		}
+		if (begin.green() > end.green() && _current.green() > end.green()) {
+			debugLineViaSerial("decrease green");
+			return client.decreaseAndSlideColor(0, 1, 0, _current);
+		}
+		// if equal => nothing to do
+		debugLineViaSerial("green matches");
+		return 0;
+	}
+
+	int slideBlue(ColorSliderClient &client, Color &begin, Color &end) {
+		if (begin.blue() < end.blue() && _current.blue() < end.blue()) {
+			debugLineViaSerial("increase blue");
+			return client.increaseAndSlideColor(0, 0, 1, _current);
+		}
+		if (begin.blue() > end.blue() && _current.blue() > end.blue()) {
+			debugLineViaSerial("decrease blue");
+			return client.decreaseAndSlideColor(0, 0, 1, _current);
+		}
+		// if equal => nothing to do
+		debugLineViaSerial("blue matches");
+		return 0;
+	}
+
 };
 
 #endif
